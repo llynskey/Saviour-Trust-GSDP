f0ea14ab6e17723ace6f7bd5a901c4dbaf0aac48
Jack <jackjdaly1@gmail.com>
2020-01-13 11:19:19 +0000

    Finished Basic Design
    Added the beginning of a light box 
    Jack and Will

--- App/routes/support.js
+++ App/routes/support.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('support', { title: 'Saviour Trust' });
+  res.render('supportWorker', { title: 'Saviour Trust' });
 });
 
 module.exports = router;
