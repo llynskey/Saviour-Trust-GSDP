edde17ae0c10ac5faa93ec3bcdee1b3fd0225a66
Unknown <lawrence1910@googlemail.com>
2020-01-15 09:52:16 +0000

    Updated login route

--- App/routes/VisitForm.js
+++ App/routes/VisitForm.js
@@ -4,14 +4,14 @@ var dbUtils = require('../controllers/DBUtils');
 
 /* GET home page. */
 router.get('/', 
-//require('connect-ensure-login').ensureLoggedIn(),
+require('connect-ensure-login').ensureLoggedIn(),
 function(req, res) {
 
   dbUtils.getHouses(function(houseIds, addressArray) {
     var houses = addressArray;
     var houseIds = houseIds;
     
-    res.render('VisitForm', { houseIds: houseIds, houses: houses});
+    res.render('VisitForm', {user: req.user, houseIds: houseIds, houses: houses});
     console.log(JSON.stringify(houses));
   });
   
